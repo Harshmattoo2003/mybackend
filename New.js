d69fd1b3c922d4f65c2be8c41e83b7321ea8695d
Harsh Mattoo <harshmattoo2003@gmail.com>
2025-04-07 21:58:17 +0530

    Overwriting with updated backend project

--- New.js
+++ New.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 // Connect MongoDB
-mongoose.connect("mongodb+srv://harshmattoo2003:Harsh1504@harsh1.w21k9rh.mongodb.net/newdb?retryWrites=true&w=majority&appName=Harsh1", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log("MongoDB Connected"))
